fec04bed6b49b3cf1dc32e862a12cac6aae3eefa
rutuja uphale <167025910+Rutuja192004@users.noreply.github.com>
2026-02-01 01:08:22 +0530

    Implement Room class and room retrieval function
    Add Room class and getRoom function for room management

--- server/rooms.js
+++ server/rooms.js
@@ -1,1 +1,24 @@
+// server/rooms.js
 
+const DrawingState = require("./drawing-state");
+
+class Room {
+  constructor(id) {
+    this.id = id;
+    this.users = new Map(); // userId -> socket
+    this.state = new DrawingState();
+    this.liveActions = new Map(); // actionId -> style
+    this.userColors = new Map(); // userId -> color
+  }
+}
+
+const rooms = new Map();
+
+function getRoom(roomId) {
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId, new Room(roomId));
+  }
+  return rooms.get(roomId);
+}
+
+module.exports = { getRoom };
